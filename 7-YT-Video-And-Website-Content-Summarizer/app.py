3087aeb4fdf308c339c4e50c5087b06381c22a3f
NickBwalley <nickbwalley@gmail.com>
2025-04-23 20:08:47 +0300

    GenAI Project: A YouTube & Website Content Summarizer based on URL

---
+++ 7-YT-Video-And-Website-Content-Summarizer/app.py
@@ -0,0 +1,59 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+
+
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YouTube or Website URL", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YouTube or Website URL")
+st.subheader('Summarize URL')
+
+
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    groq_api_key=st.text_input("Groq API Key",value="",type="password")
+
+generic_url=st.text_input("URL",label_visibility="collapsed")
+
+## Gemma Model Using Groq API
+llm =ChatGroq(model="gemma2-9b-it", groq_api_key=groq_api_key)
+
+prompt_template="""
+Provide a detailed summary of the following content. 1000 words maximum output length.:
+Content:{text}
+
+"""
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
+
+if st.button("Summarize the Content from YT or Website"):
+    ## Validate all the inputs
+    if not groq_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the information to get started")
+    elif not validators.url(generic_url):
+        st.error("Please Enter a Valid Url. It may be either a YouTube Video URL or Website URL")
+
+    else:
+        try:
+            with st.spinner("Loading..."):
+                ## loading the website or yt video data
+                if "youtube.com" in generic_url:
+                    loader=YoutubeLoader.from_youtube_url(generic_url,add_video_info=True)
+                else:
+                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
+                                                 headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                docs=loader.load()
+
+                ## Chain For Summarization
+                chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+                st.write("Loaded document:", docs)
+                output_summary=chain.run(docs)
+
+                st.success(output_summary)
+        except Exception as e:
+            import traceback
+            st.error("An error occurred while summarizing.")
+            st.code(traceback.format_exc(), language="python")
+            
